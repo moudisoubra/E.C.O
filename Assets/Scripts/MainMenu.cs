11b45e3338d603d66a71aac73d655271e7df4017
moudisoubra <moudisoubra001@gmail.com>
2019-07-16 19:15:51 +0400

    Main Menu partly done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject controlsMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Map Design");
+    }
+
+    public void GoBack()
+    {
+        mainMenu.SetActive(true);
+        controlsMenu.SetActive(false);
+    }
+
+    public void GoControls()
+    {
+        mainMenu.SetActive(false);
+        controlsMenu.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Pointer : MonoBehaviour
+{
+    public float defaultLength = 5.0f;
+    public GameObject dot;
+    public VRInputModule inputModule;
+
+    public LineRenderer lineRenderer = null;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    
+    void Update()
+    {
+        UpdateLine();   
+    }
+
+    void UpdateLine()
+    {
+        PointerEventData pointerData = inputModule.GetData();
+
+        float targetLength = pointerData.pointerCurrentRaycast.distance == 0 ? defaultLength : pointerData.pointerCurrentRaycast.distance;
+
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+
+        if (hit.collider != null)
+        {
+            endPosition = hit.point;
+        }
+
+        dot.transform.position = endPosition;
+
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, endPosition);
+    }
+
+    RaycastHit CreateRaycast(float Length)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, defaultLength);
+
+        return hit;
+
+    } 
+}
