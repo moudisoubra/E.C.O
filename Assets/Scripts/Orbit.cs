2176f9a62fc8fdc047cc826796422facc2b830f4
moudisoubra <moudisoubra001@gmail.com>
2019-07-16 21:00:34 +0400

    Main Menu 1st prototype done

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public Transform orbitAround;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+        orbitAround = GameObject.FindGameObjectWithTag("OrbitPoint").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(orbitAround.position, orbitAround.up, speed * Time.deltaTime);
+    }
+}
